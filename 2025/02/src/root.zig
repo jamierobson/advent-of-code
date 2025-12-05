b0fa9ad6930dadce94441d47980f6d320dd755be
Jamie Robson <jro@umbraco.dk>
2025-12-05 14:43:58 +0100

    Defining an initial idea of is valid. Seems to match _some_ of the input.
    More input to test

--- 2025/02/src/root.zig
+++ 2025/02/src/root.zig
@@ -1,23 +1,61 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try stdout.flush(); // Don't forget to flush!
+pub fn isValid(stringRepresentationOfNumber: []const u8) bool {
+    return !isRepeatedSequenceOfDigits(stringRepresentationOfNumber);
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+fn isRepeatedSequenceOfDigits(stringRepresentationOfNumber: []const u8) bool {
+    const s: u128 = @rem(stringRepresentationOfNumber.len, 2);
+    if (s != 0) return false;
+
+    var i: usize = 0;
+    var j: usize = stringRepresentationOfNumber.len / 2;
+
+    while (j < stringRepresentationOfNumber.len) {
+        if (stringRepresentationOfNumber[i] != stringRepresentationOfNumber[j]) return false;
+
+        i += 1;
+        j += 1;
+    }
+
+    return true;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+test "odd lengths cannot be repeated strings" {
+    const expected = false;
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("111"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("11111"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1111111"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("111111111"));
+}
+
+test "sample input from prompty 95-115 - valid" {
+    try std.testing.expectEqual(true, isValid("95"));
+    try std.testing.expectEqual(true, isValid("96"));
+    try std.testing.expectEqual(true, isValid("97"));
+    try std.testing.expectEqual(true, isValid("98"));
+
+    try std.testing.expectEqual(true, isValid("100"));
+    try std.testing.expectEqual(true, isValid("101"));
+    try std.testing.expectEqual(true, isValid("102"));
+    try std.testing.expectEqual(true, isValid("103"));
+    try std.testing.expectEqual(true, isValid("104"));
+    try std.testing.expectEqual(true, isValid("105"));
+    try std.testing.expectEqual(true, isValid("106"));
+    try std.testing.expectEqual(true, isValid("107"));
+    try std.testing.expectEqual(true, isValid("108"));
+    try std.testing.expectEqual(true, isValid("109"));
+    try std.testing.expectEqual(true, isValid("110"));
+    try std.testing.expectEqual(true, isValid("111"));
+    try std.testing.expectEqual(true, isValid("112"));
+    try std.testing.expectEqual(true, isValid("113"));
+    try std.testing.expectEqual(true, isValid("114"));
+    try std.testing.expectEqual(true, isValid("115"));
+}
+
+test "sample input from prompty 95-115 - invalid" {
+    try std.testing.expectEqual(false, isValid("99"));
 }
