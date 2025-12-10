1362d8a1cd5aead32a11b190fc406dfeb92cebc3
Jamie Robson <jro@umbraco.dk>
2025-12-10 21:53:56 +0100

    A much closer attempt to implement the part 2 - still failing

--- 2025/Day02/src/Part2.zig
+++ 2025/Day02/src/Part2.zig
@@ -19,126 +19,176 @@ fn getSumOfInvalidInRange(idGroup: types.IdGroup) u128 {
     return sum;
 }
 
-pub fn isRepeatedSequenceOfDigits(stringRepresentationOfNumber: []const u8) bool {
-    if (allCharactersAreEqual(stringRepresentationOfNumber)) {
-        return true;
-    }
-
-    for (2..stringRepresentationOfNumber.len / 2 + 1) |candidateRepetitionLength| {
-        if (isRepeatedSequenceOfDigitsOfLength(stringRepresentationOfNumber, candidateRepetitionLength)) {
+pub fn isRepeatedSequenceOfDigits(allocator: std.mem.Allocator, stringRepresentationOfNumber: []const u8) !bool {
+    var sequenceLength: u8 = 1;
+    while (sequenceLength <= (stringRepresentationOfNumber.len / 2)) {
+        if (try isRepetedSequenceOfDigitsOfLength(allocator, stringRepresentationOfNumber, sequenceLength)) {
             return true;
         }
+
+        sequenceLength += 1;
     }
 
     return false;
-
-    // if all digits are equal, return true;
-    // Otherwise Go through all divisors up to and equal len/2. if get any repetitions, then return true;
-
-    // for tryRepetitionsOfThisLength <= len/2 {
-    // if @rem(len, tryRepetitionsOfThisLength) != 0 skip, else
-    // comparativeIndices = (i, j, k, ..., z)
-    // while (lastComparativeIndex < len) {
-    // if string[i] != string[j] != string[k] != .... // Move on to the next one;
-    // (i, j, k, ..., z)++
-    // } return true (found a repetition)
-    // } return false // didn't find a single duplicate when examining everything
 }
 
-pub fn isRepeatedSequenceOfDigitsOfLength(stringRepresentationOfNumber: []const u8, candidateRepetitionLength: u8) bool {
-    const remainder: u128 = @rem(stringRepresentationOfNumber.len, candidateRepetitionLength);
-    if (remainder != 0) return false;
+fn isRepetedSequenceOfDigitsOfLength(allocator: std.mem.Allocator, stringRepresentationOfNumber: []const u8, sequenceLength: u8) !bool {
+    if (@rem(stringRepresentationOfNumber.len, sequenceLength) != 0) return false; // We can't have a repetition of a sequence if the sequence length doesn't divide the length of the number
 
-    var indices: [candidateRepetitionLength]u8 = 0 ** candidateRepetitionLength;
-    for (1..candidateRepetitionLength) |i| {
-        indices[i] = (stringRepresentationOfNumber.len * i) / candidateRepetitionLength;
+    const numberOfPartitions = stringRepresentationOfNumber.len / sequenceLength;
+
+    // Initialize indices, e.g. if we are assessing if a string of 9 characters has a repeated string of length 3, then this would be a collection of 3 items: 0, 3, 6
+    // var indices = try std.ArrayList(usize).initCapacity(allocator, numberOfPartitions);
+    var indices: std.ArrayList(usize) = .empty;
+    defer indices.deinit(allocator);
+
+    var partition: usize = 0;
+    while (partition < numberOfPartitions) {
+        try indices.append(allocator, partition * sequenceLength);
+        // indices.items[partition] = partition * sequenceLength;
+        partition += 1;
     }
 
-    while (indices[candidateRepetitionLength - 1] < stringRepresentationOfNumber.len) {
-        // Check if all bits equal each other. if false, return false,
-        // iterate and increment all indices
+    // Iterates until final index would be out of bounds.
+    while (indices.items[numberOfPartitions - 1] < stringRepresentationOfNumber.len) {
+        for (1..numberOfPartitions) |i| {
+            if (stringRepresentationOfNumber[i - 1] != stringRepresentationOfNumber[i]) {
+                return false;
+            }
+        }
+
+        // The sequence comparison still holds. Continue the comparison.
+        // Increment _all_ indices. In our example of a 9 digit number and a 3 digit sequence
+        // we iterate from (0, 3, 6) to (1, 4, 7)
+        for (0..indices.items.len) |i| {
+            indices.items[i] += 1;
+        }
     }
 
     return true;
 }
 
+// for 0 .. sequenceLength
+// for 0 .. partition
+
+// This is flawed. Todo: merge together with "all characters are equal"
+// fn isRepetedSequenceOfCharactersOfLength(allocator: std.mem.Allocator, stringRepresentationOfNumber: []const u8, candidateRepetitionLength: u8) !bool {
+//     const remainder: u128 = @rem(stringRepresentationOfNumber.len, candidateRepetitionLength);
+//     if (remainder != 0) return false;
+
+//     var indices: std.ArrayList(usize) = .empty;
+//     defer indices.deinit(allocator);
+
+//     var indexDivision: u8 = 0;
+//     while (indexDivision < candidateRepetitionLength) {
+//         try indices.append(allocator, (stringRepresentationOfNumber.len * indexDivision) / candidateRepetitionLength);
+//         indexDivision += 1;
+//     }
+
+//     while (indices.items[candidateRepetitionLength - 1] < stringRepresentationOfNumber.len) {
+
+//         //If any values are different, we return false, otherwise continue to the next indices.
+//         for (1..indices.items.len) |i| {
+//             if (stringRepresentationOfNumber[i - 1] != stringRepresentationOfNumber[i]) {
+//                 return false;
+//             }
+//         }
+
+//         // increment all indices. In our example we iterate to (1, 4, 7), then (2, 5, 8), and then (3, 6, 9)
+//         // In the while condition, 8 is not < (9-1) and so the comparison is finished, and the answer is - yes, everything is equal
+//         for (0..indices.items.len) |i| {
+//             indices.items[i] += 1;
+//         }
+//     }
+
+//     return true;
+// }
+
 fn allCharactersAreEqual(stringRepresentationOfNumber: []const u8) bool {
     for (1..stringRepresentationOfNumber.len) |i| { //exclusive upper
         if (stringRepresentationOfNumber[i - 1] != stringRepresentationOfNumber[i]) {
-            break;
-        }
-
-        if (i == stringRepresentationOfNumber.len - 1) {
-            return true;
+            return false;
         }
     }
 
-    return false;
-}
-
-test "testRange" {
-    const stringRepresentationOfNumber: []const u8 = "12345";
-
-    for (1..stringRepresentationOfNumber.len) |i| {
-        std.debug.print("\n index {any}: char {any}", .{ i, stringRepresentationOfNumber[i] - 48 });
-    }
+    return true;
 }
 
 test "any repeated number is a repeated sequence" {
     const expected = true;
 
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("33"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("333"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("3333"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("33333"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("333333"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("3333333"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("33333333"));
+    var arenaAllocator: std.heap.ArenaAllocator = .init(std.testing.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "33"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "333"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "3333"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "33333"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "333333"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "3333333"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "33333333"));
 }
 
 test "any repeated pair of numbers is a repeated sequence" {
     const expected = true;
 
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1212"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("123123"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("12341234"));
+    var arenaAllocator: std.heap.ArenaAllocator = .init(std.testing.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "1212"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "123123"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "12341234"));
 }
 
 test "any numbers repeated three times is a repeated sequence" {
     const expected = true;
 
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("121212"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("123123123"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1234123412345"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212121"));
+    var arenaAllocator: std.heap.ArenaAllocator = .init(std.testing.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "121212"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "123123123"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "1234123412345"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212121"));
 }
 
 test "any repeated pattern with any out of sequence ending is not repeated sequence" {
     const expected = false;
 
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("334"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212118"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212119"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212120"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212122"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212123"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212124"));
+    var arenaAllocator: std.heap.ArenaAllocator = .init(std.testing.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "334"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212118"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212119"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212120"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212122"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212123"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212124"));
 }
 
 test "all invalid ids from prompt" {
     const expected = true;
 
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("11"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("22"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("99"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("111"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("999"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1010"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("1188511885"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("222222"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("446446"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("38593859"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("565656"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("824824824"));
-    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits("2121212121"));
+    var arenaAllocator: std.heap.ArenaAllocator = .init(std.testing.allocator);
+    defer arenaAllocator.deinit();
+    const allocator = arenaAllocator.allocator();
+
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "11"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "22"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "99"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "111"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "999"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "1010"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "1188511885"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "222222"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "446446"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "38593859"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "565656"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "824824824"));
+    try std.testing.expectEqual(expected, isRepeatedSequenceOfDigits(allocator, "2121212121"));
 }
